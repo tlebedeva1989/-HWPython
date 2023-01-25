a68b08a9942306dc100aef6dd7057a44dd60a06e
Tlebedeva1989 <tlebedeva1989@gmail.com>
2023-01-25 14:02:13 +0300

    изменения во второй задаче

--- hw5/task2.py
+++ hw5/task2.py
@@ -26,24 +26,46 @@ def exam_win (list):
 table = [1,2,3,4,5,6,7,8,9]
 count_pl1 = 0
 count_pl2 = 0
+count = 0
 
 print_table(table)
 
-while not exam_win(table): 
-    move_pl1 = int(input(" Игрок 1 введите позицию вашего хода "))
-    table[move_pl1-1] = "X"
-    count_pl1 = 1
-    count_pl2 = 0
-    print_table(table)
+while count !=9  or  exam_win(table) == True: 
+    while count_pl1 != 1:
+        if exam_win(table) == True or count == 9: break  
+        move_pl1 = int(input(" Игрок 1 введите позицию вашего хода "))
+        if table[move_pl1-1] != "X" and table[move_pl1-1] != "O":
+            table[move_pl1-1] = "X"
+            count_pl1 = 1
+            count_pl2 = 0
+            count +=1
+            print_table(table)
+            
+          
+        else: print("позиция занята ")
+    
+        
+    if exam_win(table) == True: break  
 
-    if exam_win(table) == True: break
 
-    move_pl2 = int(input(" Игрок 2 введите позицию вашего хода "))
-    table[move_pl2-1] = "O"
-    count_pl1 = 0
-    count_pl2 = 1
-    print_table(table)
+    while count_pl2 != 1:
+        if exam_win(table) == True or count == 9: break  
+        move_pl2 = int(input(" Игрок 2 введите позицию вашего хода "))
+        
+        if table[move_pl2-1] != "X" and table[move_pl2-1] != "O":
+            table[move_pl2-1] = "O"
+            count_pl1 = 0
+            count_pl2 = 1
+            count +=1
+            print_table(table)
+            
+          
+        else: print("позиция занята ")
+        
+    
 
-if count_pl1 == 1: print("player1 Win!")
-elif count_pl2 == 1: print("player2 Win!")    
+if exam_win(table) == True: 
+    if count_pl1 == 1: print("player1 Win!")
+    elif count_pl2 == 1: print("player2 Win!") 
+else: print("Победила дружба!")   
 
